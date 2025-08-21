2cf341b15c4a917069105e0a0fb2bdbe70629e44
Sumanta Bhattacharya <148477304+SumantaBhattacharya@users.noreply.github.com>
2025-08-21 10:39:28 +0530

    Delete MIDDLEWARES directory

--- MIDDLEWARES/ExpressEroor.js
+++
@@ -1,9 +0,0 @@
-class ExpressEroor extends Error{//inheritance
-    constructor(status,message){
-        super();
-        this.status = status,
-        this.message = message
-    }
-}
-module.exports = ExpressEroor;
-//npx nodemon index.js
